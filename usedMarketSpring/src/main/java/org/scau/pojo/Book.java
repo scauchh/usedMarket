32d6cd3ebceca31508a6134f3fdbb20949344401
11828 <1182814995@qq.com>
2024-04-02 15:05:24 +0800

    修复了时间显示的bug

--- usedMarketSpring/src/main/java/org/scau/pojo/Book.java
+++ usedMarketSpring/src/main/java/org/scau/pojo/Book.java
@@ -1,5 +1,6 @@
 package org.scau.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,6 +19,7 @@ public class Book {
     private String picture;           // 书籍图片
     private Double price;             // 书籍价格
     private String type;              // 书籍类型
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime datetime;   // 数据更新时间
     private String notes;             // 备注
 }
--- usedMarketSpring/src/main/java/org/scau/service/impl/BookServiceImpl.java
+++ usedMarketSpring/src/main/java/org/scau/service/impl/BookServiceImpl.java
@@ -8,8 +8,11 @@ import org.scau.utils.ThreadLocalUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
+
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class BookServiceImpl implements BookService {
